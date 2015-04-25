a944b1c52f2f60ca543ceb2a747ab453f474d639
Nick Kullman <Nick.Kullman@gmail.com>
2015-04-25 12:41:02 -0700

    Changed weather data pull to daily
    Now using daily-summarized weather data

--- getWeatherData.r
+++ getWeatherData.r
@@ -5,14 +5,11 @@
 library("weatherData")
 
 # Get weather data
-fremontWeatherData <- getWeatherForDate(station_id = "KBFI",
+fremontWeatherData <- getSummarizedWeather(station_id = "KBFI",
                                         start_date = "2012-10-01",
                                         end_date = "2015-04-23",
-                                        opt_detailed = TRUE,
-                                        opt_write_to_file = FALSE,
-                                        opt_temperature_columns = TRUE,
                                         opt_all_columns = TRUE,
                                         opt_verbose = TRUE)
 										
 # Write raw output to file
-write.csv(fremontWeatherData, file = "FremontWeatherData_raw.csv")
+write.csv(fremontWeatherData, file = "FremontWeatherData_Daily_raw.csv")
